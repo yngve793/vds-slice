b149dc24030604cd0271d1b67c1f491e1283ac07
Yngve Villanger <yvi@equinor.com>
2023-11-08 10:12:55 +0100

    Add 2x value of 10_samples_default

--- internal/core/core_general_test.go
+++ internal/core/core_general_test.go
@@ -20,6 +20,7 @@ var well_known_custom_axis_order = make_connection("well_known/well_known_custom
 var invalid_axes_names = make_connection("invalid_data/invalid_axes_names.vds")
 var well_known = make_connection("well_known/well_known_default.vds")
 var samples10 = make_connection("10_samples/10_samples_default.vds")
+var samples10_2x = make_connection("10_samples/10_samples_default_2x.vds")
 var prestack = make_connection("prestack/prestack_default.vds")
 
 var fillValue = float32(-999.25)
