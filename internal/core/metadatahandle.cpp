15371b50599df0100119d81ab12dbea025adbcd1
Yngve Villanger <yvi@equinor.com>
2023-11-15 08:18:07 +0100

    Handle double metadata separately

--- internal/core/metadatahandle.cpp
+++ internal/core/metadatahandle.cpp
@@ -1,24 +1,78 @@
 #include "metadatahandle.hpp"
 
-#include <stdexcept>
-#include <list>
-#include <utility>
-#include <boost/algorithm/string/join.hpp>
 #include <OpenVDS/KnownMetadata.h>
+#include <boost/algorithm/string/join.hpp>
+#include <list>
+#include <stdexcept>
+#include <utility>
 
 #include "axis.hpp"
 #include "boundingbox.hpp"
 #include "direction.hpp"
 
-SingleMetadataHandle::SingleMetadataHandle(OpenVDS::VolumeDataLayout const * const layout)
+SingleMetadataHandle::SingleMetadataHandle(OpenVDS::VolumeDataLayout const* const layout)
     : m_layout(layout),
-      m_iline( Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Inline())}))),
-      m_xline( Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Crossline())}))),
-      m_sample(Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Sample()),
-                                           std::string(OpenVDS::KnownAxisNames::Depth()),
-                                           std::string(OpenVDS::KnownAxisNames::Time())})))
-{
+      m_iline(Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Inline())}))),
+      m_xline(Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Crossline())}))),
+      m_sample(Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Sample()), std::string(OpenVDS::KnownAxisNames::Depth()), std::string(OpenVDS::KnownAxisNames::Time())}))) {
     this->dimension_validation();
+}
+
+Axis SingleMetadataHandle::iline() const noexcept(true) {
+    return this->m_iline;
+}
+
+Axis SingleMetadataHandle::xline() const noexcept(true) {
+    return this->m_xline;
+}
+
+Axis SingleMetadataHandle::sample() const noexcept(true) {
+    return this->m_sample;
+}
+
+Axis SingleMetadataHandle::get_axis(
+    Direction const direction
+) const noexcept(false) {
+    if (direction.is_iline())
+        return this->iline();
+    else if (direction.is_xline())
+        return this->xline();
+    else if (direction.is_sample())
+        return this->sample();
+
+    throw std::runtime_error("Unhandled axis");
+}
+
+BoundingBox SingleMetadataHandle::bounding_box() const noexcept(false) {
+    return BoundingBox(
+        this->iline().nsamples(),
+        this->xline().nsamples(),
+        this->coordinate_transformer()
+    );
+}
+
+std::string SingleMetadataHandle::crs() const noexcept(false) {
+    auto const crs = OpenVDS::KnownMetadata::SurveyCoordinateSystemCRSWkt();
+    return this->m_layout->GetMetadataString(crs.GetCategory(), crs.GetName());
+}
+
+std::string SingleMetadataHandle::input_filename() const noexcept(false) {
+    auto const disp_name = OpenVDS::KnownMetadata::ImportInformationInputFileName();
+    return this->m_layout->GetMetadataString(disp_name.GetCategory(), disp_name.GetName());
+}
+
+std::string SingleMetadataHandle::import_time_stamp() const noexcept(false) {
+    auto const time_stamp = OpenVDS::KnownMetadata::ImportInformationImportTimeStamp();
+    return this->m_layout->GetMetadataString(time_stamp.GetCategory(), time_stamp.GetName());
+}
+
+OpenVDS::IJKCoordinateTransformer SingleMetadataHandle::coordinate_transformer()
+    const noexcept(true) {
+    return OpenVDS::IJKCoordinateTransformer(this->m_layout);
+}
+
+OpenVDS::VolumeDataLayout const* const SingleMetadataHandle::get_layout() const noexcept(false) {
+    return this->m_layout;
 }
 
 void SingleMetadataHandle::dimension_validation() const {
@@ -30,7 +84,7 @@ void SingleMetadataHandle::dimension_validation() const {
     }
 }
 
-int SingleMetadataHandle::get_dimension(std::vector<std::string> const & names) const {
+int SingleMetadataHandle::get_dimension(std::vector<std::string> const& names) const {
     for (auto i = 0; i < this->m_layout->GetDimensionality(); i++) {
         std::string dimension_name = this->m_layout->GetDimensionName(i);
         if (std::find(names.begin(), names.end(), dimension_name) != names.end()) {
@@ -39,107 +93,90 @@ int SingleMetadataHandle::get_dimension(std::vector<std::string> const & names) 
     }
     throw std::runtime_error(
         "Requested axis not found under names " + boost::algorithm::join(names, ", ") +
-        " in vds file ");
-}
-
-Axis SingleMetadataHandle::iline() const noexcept (true) {
-    return this->m_iline;
-}
-
-Axis SingleMetadataHandle::xline() const noexcept (true) {
-    return this->m_xline;
-}
-
-Axis SingleMetadataHandle::sample() const noexcept (true) {
-    return this->m_sample;
-}
-
-BoundingBox SingleMetadataHandle::bounding_box() const noexcept (true) {
-    return BoundingBox(
-        this->iline().nsamples(),
-        this->xline().nsamples(),
-        this->coordinate_transformer()
+        " in vds file "
     );
-}
-
-std::string SingleMetadataHandle::crs() const noexcept (true) {
-    auto const crs = OpenVDS::KnownMetadata::SurveyCoordinateSystemCRSWkt();
-    return this->m_layout->GetMetadataString(crs.GetCategory(), crs.GetName());
-}
-
-std::string SingleMetadataHandle::input_filename() const noexcept (true) {
-    auto const disp_name = OpenVDS::KnownMetadata::ImportInformationInputFileName();
-    return this->m_layout->GetMetadataString(disp_name.GetCategory(), disp_name.GetName());
-}
-
-std::string SingleMetadataHandle::import_time_stamp() const noexcept (true) {
-    auto const time_stamp = OpenVDS::KnownMetadata::ImportInformationImportTimeStamp();
-    return this->m_layout->GetMetadataString(time_stamp.GetCategory(), time_stamp.GetName());
-}
-
-Axis SingleMetadataHandle::get_axis(
-    Direction const direction
-) const noexcept (false) {
-    if      (direction.is_iline())  return this->iline();
-    else if (direction.is_xline())  return this->xline();
-    else if (direction.is_sample()) return this->sample();
-
-    throw std::runtime_error("Unhandled axis");
-}
-
-OpenVDS::IJKCoordinateTransformer SingleMetadataHandle::coordinate_transformer()
-    const noexcept (true)
-{
-    return OpenVDS::IJKCoordinateTransformer(this->m_layout);
 }
 
 DoubleMetadataHandle::DoubleMetadataHandle(
     MetadataHandle const& handle_A,
-    MetadataHandle const& handle_B)
+    MetadataHandle const& handle_B
+)
     : m_handle_A(&handle_A),
-      m_handle_B(&handle_B)
-{
-
+      m_handle_B(&handle_B) {
+    this->validate_metadata();
 }
 
 Axis DoubleMetadataHandle::iline() const noexcept(true) {
+    // Axis iline in handle A and B are identical by validate_metadata()
     return this->m_handle_A->iline();
 }
 
 Axis DoubleMetadataHandle::xline() const noexcept(true) {
+    // Axis xline in handle A and B are identical by validate_metadata()
     return this->m_handle_A->xline();
 }
 
 Axis DoubleMetadataHandle::sample() const noexcept(true) {
+    // Axis sample in handle A and B are identical by validate_metadata()
     return this->m_handle_A->sample();
-}
-
-BoundingBox DoubleMetadataHandle::bounding_box() const noexcept(true) {
-    return this->m_handle_A->bounding_box();
-}
-
-std::string DoubleMetadataHandle::crs() const noexcept(true) {
-    return this->m_handle_A->crs();
-}
-
-std::string DoubleMetadataHandle::input_filename() const noexcept(true) {
-    return this->m_handle_A->input_filename();
-}
-
-std::string DoubleMetadataHandle::import_time_stamp() const noexcept(true) {
-    return this->m_handle_A->import_time_stamp();
 }
 
 Axis DoubleMetadataHandle::get_axis(
     Direction const direction
 ) const noexcept(false) {
-    if      (direction.is_iline())  return this->iline();
-    else if (direction.is_xline())  return this->xline();
-    else if (direction.is_sample()) return this->sample();
+    if (direction.is_iline())
+        return this->iline();
+    else if (direction.is_xline())
+        return this->xline();
+    else if (direction.is_sample())
+        return this->sample();
 
     throw std::runtime_error("Unhandled axis");
 }
 
+BoundingBox DoubleMetadataHandle::bounding_box() const noexcept(false) {
+    throw std::runtime_error("Not implemented");
+    return this->m_handle_A->bounding_box();
+}
+
+std::string DoubleMetadataHandle::crs() const noexcept(false) {
+    throw std::runtime_error("Not implemented");
+    return this->m_handle_A->crs();
+}
+
+std::string DoubleMetadataHandle::input_filename() const noexcept(false) {
+    throw std::runtime_error("Not implemented");
+    return this->m_handle_A->input_filename();
+}
+
+std::string DoubleMetadataHandle::import_time_stamp() const noexcept(false) {
+    throw std::runtime_error("Not implemented");
+    return this->m_handle_A->import_time_stamp();
+}
+
 OpenVDS::IJKCoordinateTransformer DoubleMetadataHandle::coordinate_transformer() const noexcept(true) {
     return OpenVDS::IJKCoordinateTransformer();
+}
+
+OpenVDS::VolumeDataLayout const* const DoubleMetadataHandle::get_layout() const noexcept(false) {
+    throw std::runtime_error("Not implemented");
+    return this->m_handle_A->get_layout();
+}
+
+void DoubleMetadataHandle::validate_metadata() const noexcept(false) {
+    if (this->m_handle_A->get_layout()->GetDimensionality() != 3) {
+        throw std::runtime_error(
+            "Unsupported VDS, expected 3 dimensions, got " +
+            std::to_string(this->m_handle_A->get_layout()->GetDimensionality())
+        );
+    }
+    if (this->m_handle_B->get_layout()->GetDimensionality() != 3) {
+        throw std::runtime_error(
+            "Unsupported VDS, expected 3 dimensions, got " +
+            std::to_string(this->m_handle_B->get_layout()->GetDimensionality())
+        );
+    }
+    this->m_handle_A->iline().validate_compatible(this->m_handle_B->iline());
+    this->m_handle_A->xline().validate_compatible(this->m_handle_B->xline());
+    this->m_handle_A->sample().validate_compatible(this->m_handle_B->sample());
 }
--- internal/core/metadatahandle.hpp
+++ internal/core/metadatahandle.hpp
@@ -1,86 +1,81 @@
 #ifndef VDS_SLICE_METADATAHANDLE_HPP
 #define VDS_SLICE_METADATAHANDLE_HPP
 
-#include <string>
-
 #include <OpenVDS/OpenVDS.h>
+#include <string>
 
 #include "axis.hpp"
 #include "boundingbox.hpp"
 #include "direction.hpp"
 
-class MetadataHandle{
+class MetadataHandle {
 public:
-    // virtual MetadataHandle(OpenVDS::VolumeDataLayout const * const layout)=0;
+    virtual Axis iline() const noexcept(true) = 0;
+    virtual Axis xline() const noexcept(true) = 0;
+    virtual Axis sample() const noexcept(true) = 0;
+    virtual Axis get_axis(Direction const direction) const noexcept(false) = 0;
 
-    virtual Axis iline()  const noexcept (true)=0;
-    virtual Axis xline()  const noexcept (true)=0;
-    virtual Axis sample() const noexcept (true)= 0;
+    virtual BoundingBox bounding_box() const noexcept(false) = 0;
+    virtual std::string crs() const noexcept(false) = 0;
+    virtual std::string input_filename() const noexcept(false) = 0;
+    virtual std::string import_time_stamp() const noexcept(false) = 0;
 
-    virtual BoundingBox bounding_box()      const noexcept (true)= 0;
-    virtual std::string crs()               const noexcept (true)= 0;
-    virtual std::string input_filename()    const noexcept (true)= 0;
-    virtual std::string import_time_stamp() const noexcept (true)= 0;
-
-    virtual Axis get_axis(Direction const direction) const noexcept (false)= 0;
-
-    virtual OpenVDS::IJKCoordinateTransformer coordinate_transformer() const noexcept (true)= 0;
-
+    virtual OpenVDS::IJKCoordinateTransformer coordinate_transformer() const noexcept(true) = 0;
+    virtual OpenVDS::VolumeDataLayout const* const get_layout() const noexcept(false) = 0;
 };
 
-class SingleMetadataHandle : public MetadataHandle{
+class SingleMetadataHandle : public MetadataHandle {
 public:
-    SingleMetadataHandle(OpenVDS::VolumeDataLayout const * const layout);
+    SingleMetadataHandle(OpenVDS::VolumeDataLayout const* const layout);
 
-    Axis iline()  const noexcept (true);
-    Axis xline()  const noexcept (true);
-    Axis sample() const noexcept (true);
+    Axis iline() const noexcept(true);
+    Axis xline() const noexcept(true);
+    Axis sample() const noexcept(true);
+    Axis get_axis(Direction const direction) const noexcept(false);
 
-    BoundingBox bounding_box()      const noexcept (true);
-    std::string crs()               const noexcept (true);
-    std::string input_filename()    const noexcept (true);
-    std::string import_time_stamp() const noexcept (true);
+    BoundingBox bounding_box() const noexcept(false);
+    std::string crs() const noexcept(false);
+    std::string input_filename() const noexcept(false);
+    std::string import_time_stamp() const noexcept(false);
 
-    Axis get_axis(Direction const direction) const noexcept (false);
+    OpenVDS::IJKCoordinateTransformer coordinate_transformer() const noexcept(true);
+    OpenVDS::VolumeDataLayout const* const get_layout() const noexcept(false);
 
-    OpenVDS::IJKCoordinateTransformer coordinate_transformer() const noexcept (true);
 private:
-    OpenVDS::VolumeDataLayout const * const m_layout;
+    OpenVDS::VolumeDataLayout const* const m_layout;
 
     Axis m_iline;
     Axis m_xline;
     Axis m_sample;
 
     void dimension_validation() const;
-    int get_dimension(std::vector<std::string> const & names) const;
+    int get_dimension(std::vector<std::string> const& names) const;
 };
 
 class DoubleMetadataHandle : public MetadataHandle {
 public:
     DoubleMetadataHandle(
         MetadataHandle const& handle_A,
-        MetadataHandle const& handle_B);
+        MetadataHandle const& handle_B
+    );
 
-    Axis iline()  const noexcept (true);
-    Axis xline()  const noexcept (true);
-    Axis sample() const noexcept (true);
+    Axis iline() const noexcept(true);
+    Axis xline() const noexcept(true);
+    Axis sample() const noexcept(true);
+    Axis get_axis(Direction const direction) const noexcept(false);
 
-    BoundingBox bounding_box()      const noexcept (true);
-    std::string crs()               const noexcept (true);
-    std::string input_filename()    const noexcept (true);
-    std::string import_time_stamp() const noexcept (true);
+    BoundingBox bounding_box() const noexcept(false);
+    std::string crs() const noexcept(false);
+    std::string input_filename() const noexcept(false);
+    std::string import_time_stamp() const noexcept(false);
 
-    Axis get_axis(Direction const direction) const noexcept (false);
-
-    OpenVDS::IJKCoordinateTransformer coordinate_transformer() const noexcept (true);
+    OpenVDS::IJKCoordinateTransformer coordinate_transformer() const noexcept(true);
+    OpenVDS::VolumeDataLayout const* const get_layout() const noexcept(false);
 
 private:
-    MetadataHandle const * m_handle_A;
-    MetadataHandle const * m_handle_B;
+    MetadataHandle const* m_handle_A;
+    MetadataHandle const* m_handle_B;
 
-    // Axis m_iline;
-    // Axis m_xline;
-    // Axis m_sample;
-
+    void validate_metadata() const noexcept(false);
 };
 #endif /* VDS_SLICE_METADATAHANDLE_HPP */
