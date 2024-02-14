9e372d52aaddc1a51e0dc62ac9bae67c596edc6d
Yngve Villanger <yvi@equinor.com>
2024-02-14 13:24:11 +0100

    Introduce SingleAxis

--- internal/core/axis.hpp
+++ internal/core/axis.hpp
@@ -25,6 +25,33 @@ public:
     virtual void assert_equal(BaseAxis const& other) noexcept(false) = 0;
 };
 
+class SingleAxis : public BaseAxis {
+
+public:
+    SingleAxis(OpenVDS::VolumeDataLayout const* const layout, int const dimension);
+
+    int nsamples() const noexcept(true);
+
+    float min() const noexcept(true);
+    float max() const noexcept(true);
+
+    float stepsize() const noexcept(true);
+
+    std::string unit() const noexcept(true);
+    int dimension() const noexcept(true);
+
+    std::string name() const noexcept(true);
+
+    bool inrange(float coordinate) const noexcept(true);
+    float to_sample_position(float coordinate) const noexcept(false);
+
+    void assert_equal(BaseAxis const& other) noexcept(false);
+
+private:
+    int const m_dimension;
+    OpenVDS::VolumeDataAxisDescriptor m_axis_descriptor;
+};
+
 class Axis {
 public:
     Axis(
