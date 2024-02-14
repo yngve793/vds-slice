c045ff8350752954f767f0b5004577bcf1f68305
Yngve Villanger <yvi@equinor.com>
2024-02-14 09:51:21 +0100

    Format files in accordance with VS Code C++ style
    All changes are machine generated. The purpose of the change is to use a
    consistent style for the whole file.

--- internal/core/datahandle.hpp
+++ internal/core/datahandle.hpp
@@ -13,41 +13,40 @@ using voxel = float[OpenVDS::Dimensionality_Max];
 
 class DataHandle {
     DataHandle(OpenVDS::VDSHandle handle);
-    friend DataHandle* make_datahandle(const char* url,const char* credentials);
+    friend DataHandle* make_datahandle(const char* url, const char* credentials);
+
 public:
+    MetadataHandle const& get_metadata() const noexcept(true);
 
-    MetadataHandle const& get_metadata() const noexcept (true);
+    static OpenVDS::VolumeDataFormat format() noexcept(true);
 
-    static OpenVDS::VolumeDataFormat format() noexcept (true);
-
-    std::int64_t subcube_buffer_size(SubCube const& subcube) noexcept (false);
+    std::int64_t subcube_buffer_size(SubCube const& subcube) noexcept(false);
 
     void read_subcube(
-        void * const buffer,
+        void* const buffer,
         std::int64_t size,
         SubCube const& subcube
-    ) noexcept (false);
+    ) noexcept(false);
 
-    std::int64_t traces_buffer_size(std::size_t const ntraces) noexcept (false);
+    std::int64_t traces_buffer_size(std::size_t const ntraces) noexcept(false);
 
     void read_traces(
-        void * const                    buffer,
-        std::int64_t const              size,
-        voxel const*                    coordinates,
-        std::size_t const               ntraces,
+        void* const buffer,
+        std::int64_t const size,
+        voxel const* coordinates,
+        std::size_t const ntraces,
         enum interpolation_method const interpolation_method
-    ) noexcept (false);
+    ) noexcept(false);
 
-
-    std::int64_t samples_buffer_size(std::size_t const nsamples) noexcept (false);
+    std::int64_t samples_buffer_size(std::size_t const nsamples) noexcept(false);
 
     void read_samples(
-        void * const                    buffer,
-        std::int64_t const              size,
-        voxel const*                    samples,
-        std::size_t const               nsamples,
+        void* const buffer,
+        std::int64_t const size,
+        voxel const* samples,
+        std::size_t const nsamples,
         enum interpolation_method const interpolation_method
-    ) noexcept (false);
+    ) noexcept(false);
 
 private:
     OpenVDS::ScopedVDSHandle m_file_handle;
@@ -61,7 +60,6 @@ private:
 DataHandle* make_datahandle(
     const char* url,
     const char* credentials
-) noexcept (false);
-
+) noexcept(false);
 
 #endif /* VDS_SLICE_DATAHANDLE_HPP */
