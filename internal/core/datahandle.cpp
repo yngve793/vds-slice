837ae743151bf38a73ab7fd4b77bbb4f2aa1322d
Yngve Villanger <yvi@equinor.com>
2024-02-14 09:51:21 +0100

    Format files in accordance with VS Code C++ style
    All changes are machine generated. The purpose of the change is to use a
    consistent style for the whole file.

--- internal/core/datahandle.cpp
+++ internal/core/datahandle.cpp
@@ -11,13 +11,17 @@
 namespace {
 
 OpenVDS::InterpolationMethod to_interpolation(interpolation_method interpolation) {
-    switch (interpolation)
-    {
-        case NEAREST: return OpenVDS::InterpolationMethod::Nearest;
-        case LINEAR: return OpenVDS::InterpolationMethod::Linear;
-        case CUBIC: return OpenVDS::InterpolationMethod::Cubic;
-        case ANGULAR: return OpenVDS::InterpolationMethod::Angular;
-        case TRIANGULAR: return OpenVDS::InterpolationMethod::Triangular;
+    switch (interpolation) {
+        case NEAREST:
+            return OpenVDS::InterpolationMethod::Nearest;
+        case LINEAR:
+            return OpenVDS::InterpolationMethod::Linear;
+        case CUBIC:
+            return OpenVDS::InterpolationMethod::Cubic;
+        case ANGULAR:
+            return OpenVDS::InterpolationMethod::Angular;
+        case TRIANGULAR:
+            return OpenVDS::InterpolationMethod::Triangular;
         default: {
             throw std::runtime_error("Unhandled interpolation method");
         }
@@ -32,23 +36,20 @@ DataHandle* make_datahandle(
 ) {
     OpenVDS::Error error;
     auto handle = OpenVDS::Open(url, credentials, error);
-    if(error.code != 0) {
+    if (error.code != 0) {
         throw std::runtime_error("Could not open VDS: " + error.string);
     }
     return new DataHandle(std::move(handle));
 }
 
 DataHandle::DataHandle(OpenVDS::VDSHandle handle)
-    : m_file_handle(handle)
-    , m_access_manager(OpenVDS::GetAccessManager(handle))
-    , m_metadata(m_access_manager.GetVolumeDataLayout())
-{}
+    : m_file_handle(handle), m_access_manager(OpenVDS::GetAccessManager(handle)), m_metadata(m_access_manager.GetVolumeDataLayout()) {}
 
-MetadataHandle const& DataHandle::get_metadata() const noexcept (true) {
+MetadataHandle const& DataHandle::get_metadata() const noexcept(true) {
     return this->m_metadata;
 }
 
-OpenVDS::VolumeDataFormat DataHandle::format() noexcept (true) {
+OpenVDS::VolumeDataFormat DataHandle::format() noexcept(true) {
     /*
      * We always want to request data in OpenVDS::VolumeDataFormat::Format_R32
      * format for slice. For fence documentation says: "The traces/samples are
@@ -59,7 +60,7 @@ OpenVDS::VolumeDataFormat DataHandle::format() noexcept (true) {
 
 std::int64_t DataHandle::subcube_buffer_size(
     SubCube const& subcube
-) noexcept (false) {
+) noexcept(false) {
     std::int64_t size = this->m_access_manager.GetVolumeSubsetBufferSize(
         subcube.bounds.lower,
         subcube.bounds.upper,
@@ -72,10 +73,10 @@ std::int64_t DataHandle::subcube_buffer_size(
 }
 
 void DataHandle::read_subcube(
-    void * const buffer,
+    void* const buffer,
     std::int64_t size,
     SubCube const& subcube
-) noexcept (false) {
+) noexcept(false) {
     auto request = this->m_access_manager.RequestVolumeSubset(
         buffer,
         size,
@@ -93,18 +94,18 @@ void DataHandle::read_subcube(
     }
 }
 
-std::int64_t DataHandle::traces_buffer_size(std::size_t const ntraces) noexcept (false) {
+std::int64_t DataHandle::traces_buffer_size(std::size_t const ntraces) noexcept(false) {
     int const dimension = this->get_metadata().sample().dimension();
     return this->m_access_manager.GetVolumeTracesBufferSize(ntraces, dimension);
 }
 
 void DataHandle::read_traces(
-    void * const                    buffer,
-    std::int64_t const              size,
-    voxel const*                    coordinates,
-    std::size_t const               ntraces,
+    void* const buffer,
+    std::int64_t const size,
+    voxel const* coordinates,
+    std::size_t const ntraces,
     enum interpolation_method const interpolation_method
-) noexcept (false) {
+) noexcept(false) {
     int const dimension = this->get_metadata().sample().dimension();
 
     auto request = this->m_access_manager.RequestVolumeTraces(
@@ -127,7 +128,7 @@ void DataHandle::read_traces(
 
 std::int64_t DataHandle::samples_buffer_size(
     std::size_t const nsamples
-) noexcept (false) {
+) noexcept(false) {
     return this->m_access_manager.GetVolumeSamplesBufferSize(
         nsamples,
         DataHandle::channel
@@ -135,12 +136,12 @@ std::int64_t DataHandle::samples_buffer_size(
 }
 
 void DataHandle::read_samples(
-    void * const                    buffer,
-    std::int64_t const              size,
-    voxel const*                    samples,
-    std::size_t const               nsamples,
+    void* const buffer,
+    std::int64_t const size,
+    voxel const* samples,
+    std::size_t const nsamples,
     enum interpolation_method const interpolation_method
-) noexcept (false) {
+) noexcept(false) {
     auto request = this->m_access_manager.RequestVolumeSamples(
         (float*)buffer,
         size,
