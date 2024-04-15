bc83fc44b6998bd4ae9c08d4534c0bec70b6a590
Yngve Villanger <yvi@equinor.com>
2024-04-15 15:24:16 +0200

    Start using DoubleVolumeDataLayout

--- api/request.go
+++ api/request.go
@@ -67,7 +67,7 @@ type RequestedResource struct {
 	// Valid options are: "addition", "subtraction", "multiplication", "division" and empty string ("").
 	//
 	// Note that there are some restrictions when applying a binary operation on two cubes.
-	// Each of the axes inline, crossline and depth/time must be identical.
+	// The axes inline, crossline and depth/time must be in the same order.
 	// I.e, same start value, same stepsize and same number of values.
 	// Further more the axis must be of the same type, for instance it is not possible to take the
 	// difference between a time cube and a depth cube.
